022cb52f8a71c2adbf916ff4431c11ca23db9fba
dmrb <d.m.r.buencon@gmail.com>
2025-02-25 16:01:10 +0100

    ejercicio 3

--- Fechas.py
+++ Fechas.py
@@ -1,10 +1,8 @@
 import re
 from re import findall
 
-expresion = r'\d{4}\-\d{2}\-\d{2}\s'
+expresion = r'(\d{4})\-(\d{2})\-(\d{2})\s'
 
 texto = input()
-results = re.findall(expresion, texto)
-for r in results:
-    print(r)
-
+texto2 = re.sub(expresion, r"\3.\2.\1 ", texto)
+print(texto2)
