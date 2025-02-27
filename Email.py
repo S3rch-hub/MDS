8ebf61a46e8ed8cc41ebf855fdd299749d3f2133
Sergio Morales Bascuñana <sermoba8@gmail.com>
2025-02-27 17:54:21 +0100

    Email hecho

--- Email.py
+++ Email.py
@@ -2,15 +2,14 @@ import re
 from re import findall
 
 alumnos = r'\b([a-z])\.([a-z]{2,})\.(\d{4})@alumnos\.urjc\.es\b'
-profesores = r'\b([a-z]{2,})\.([a-z]{2,})@urjc\.es\b'
+profesores = r'\b([a-z]+)\.([a-z]{2,})@urjc\.es\b'
 texto = input()
 
-matchAlumnos = re.search(alumnos,texto)
-matchProfesores = re.search(profesores,texto)
-if matchAlumnos:
-    inicial,apellido,anio= matchAlumnos.groups()
-    print("alumno "+ apellido + " matriculado en "+ anio )
-if matchProfesores:
-    nombre,apellido= matchProfesores.groups()
-    print("profesor "+ nombre+ " apellido "+ apellido)
+
+matchAlumnos = re.findall(alumnos,texto)
+matchProfesores = re.findall(profesores,texto)
+for inicial,apellido,anio in  matchAlumnos:
+    print(f"alumno "+ apellido + " matriculado en "+ anio )
+for nombre,apellido in matchProfesores:
+    print(f"profesor "+ nombre+ " apellido "+ apellido)
 
