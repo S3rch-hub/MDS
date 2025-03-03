4c1aa8c6b9ba23efd7be2a1eff24753dfde685e9
dmrb <d.m.r.buencon@gmail.com>
2025-03-03 18:49:40 +0100

    ejercicio 5

--- Logs.py
+++ Logs.py
@@ -3,7 +3,7 @@ from re import findall
 
 texto = input()
 
-expresion = r'(?P<time>\S+)\s+(?P<level>\bINFO\b|\bERROR\b|\bDEBUG\b|\bWARN\b)\s+\d+\s+---\s+\[(?P<thread>[\w\d]+)]\s+(?P<class_name>[\w\.]+)\s+:\s+(?P<message>.*)'
+expresion = r'(?:\S+)\s+(\bINFO\b|\bERROR\b|\bDEBUG\b|\bWARN\b)\s+\d+\s+---\s+\[([\w\d]+)]\s+(?:(?:\w+\.)+)?(\S+)\s+:\s+(.*)'
 salida = re.findall(expresion, texto)
-for time,level,thread,class_name,message in salida:
-    print(f'"{level}"," {thread} ","{class_name}","{message}')
+for level,thread,class_name,message in salida:
+    print(f'"{level}","{thread}","{class_name}","{message}"')
